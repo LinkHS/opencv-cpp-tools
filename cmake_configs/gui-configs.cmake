9c10a1dca570f6fb4a19009bb3bb54f422017754
Dawei.Yang <381082014@qq.com>
2018-04-17 21:23:30 +0800

    add first project camera-recorder

---
+++ cmake_configs/gui-configs.cmake
@@ -0,0 +1,4 @@
+# OpenCV
+set (OPENCV_INC_PATH  "Z:/Downloads/opencv/build/include" CACHE PATH  "OpenCV Include Directory")
+set (OPENCV_LIB_PATH  "Z:/Downloads/opencv/build/x64/vc14/lib"  CACHE PATH  "OpenCV Library Directory")
+set (OPENCV_LIBS  "opencv_world341" CACHE STRING "OpenCV Libraries to be linked")
